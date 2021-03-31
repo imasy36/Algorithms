dc05bf6c8585002df8659e3dc71422ff1a1e9141
AvinashxU <imasy36@gmail.com>
2021-03-31 14:05:57 +0530

    Added Ologn implementation of array inversion count

--- count_inversions.py
+++ count_inversions.py
@@ -1,4 +1,4 @@
-## count inversions in an array
+## counting inversions in an array
 
 def count_inv(arr:list):
 	count = 0
@@ -8,6 +8,50 @@ def count_inv(arr:list):
 				count+=1
 	return count
 
+
+def mergeAndCountInv(b:list, c:list):
+	result = []
+	count  = 0 
+	l = len(b) + len(c)
+	i=j=k=0
+	while i<len(b) and j<len(c):
+		if b[i]<c[j]:
+			result.append(b[i])
+			i+=1
+		else:
+			result.append(c[j])
+			j+=1
+			if k<l//2:
+				count+= len(b) - i
+		k+=1
+
+	while i<len(b):
+		result.append(b[i])
+		i+=1
+		k+=1
+	
+	while j<len(c):
+		result.append(c[j])
+		j+=1
+		k+=1
+
+	return result, count
+
+
+def countAndSortInv(arr:list):
+	if len(arr)==1:
+		return [arr, 0]
+	n = len(arr)
+	b, count_b = countAndSortInv(arr[:n//2])
+	c, count_c = countAndSortInv(arr[n//2:])
+	d, count_d = mergeAndCountInv(b, c)
+
+	## print(d," - ",count_b + count_c + count_d)
+	return [d, count_b + count_c + count_d]
+
+
 if __name__=='__main__':
 	arr = [int(x) for x in input('Enter elements: ').split()]
-	print("Total inversions: ".format(count_inv(arr)))
+	print("Total inversions: {}  --O(n^2)".format(count_inv(arr)))
+	print("Total inversions: {}  --O(nlogn)".format(countAndSortInv(arr)[1]))
+	
